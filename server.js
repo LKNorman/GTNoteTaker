293c80fe1c59523339075c6b016977ac44aff5b3
LKNorman <lknorman30@gmail.com>
2020-04-04 00:04:57 -0400

    comment

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 let notesJSON = require("./db/db.json");
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+// funtion to randomise the db ids
 function uuidv4() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
       var r = Math.random() * 16 + new Date().getTime | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
